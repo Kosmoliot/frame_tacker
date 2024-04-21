46cea6767a26dfacbc10e9c2af59dbe52dc618c3
Miguel <michail.pliuscik@gmail.com>
2024-04-21 20:49:32 +0100

    working code to extract values from the video and output it ot txt file

--- scraper_app.py
+++ scraper_app.py
@@ -5,7 +5,12 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd = '/opt/homebrew/Cellar/tesseract/5.3.4_1/bin/tesseract'
 
 # Open the video file or capture video from camera
-cap = cv2.VideoCapture('video/short_sample.mp4')
+cap = cv2.VideoCapture('video/sample.mp4')
+
+# Define the coordinates of the region of interest (ROI)
+roi_x, roi_y, roi_width, roi_height = 350, 150, 1500, 150
+
+output_file = open('output_file.txt', 'w') 
 
 # Initialize frame counter
 frame_count = 0
@@ -14,9 +19,12 @@ while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
+
+    # Crop the frame to extract the ROI
+    roi = frame[roi_y:roi_y+roi_height, roi_x:roi_x+roi_width]
     
     # Preprocess the frame (convert to grayscale)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
     
     # Use Tesseract OCR to recognize numbers
     numbers = pytesseract.image_to_string(gray, config='--psm 6 digits')
@@ -24,19 +32,21 @@ while cap.isOpened():
     output = {}
     # Display or save the recognized numbers
     if numbers:
-        output = {frame_count: {numbers.strip()}}
+        output_file.write(f"Frame {frame_count}: Recognized numbers in ROI: {numbers.strip()}\n")
     
-    print(output)
     # Increment frame counter
     frame_count += 1
     
-    # Display the frame
-    cv2.imshow('Video', frame)
+    # Display the frame with ROI
+    cv2.rectangle(frame, (roi_x, roi_y), (roi_x + roi_width, roi_y + roi_height), (0, 255, 0), 2)
+    cv2.imshow('Video with ROI', frame)
     
     # Press 'q' to exit
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
 
+# Close the output file
+output_file.close()
 # Release resources
 cap.release()
 cv2.destroyAllWindows()
