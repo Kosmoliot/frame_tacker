e5b5d724097525f99f0e94f4a97e4afb83ea9c25
Miguel <michail.pliuscik@gmail.com>
2024-04-26 10:06:15 +0100

    output result to a csv file

--- tracker_app.py
+++ tracker_app.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
+import csv
 
 # Capture video from camera or file
-cap = cv2.VideoCapture('video/test.MOV')
+cap = cv2.VideoCapture('video/ski.mp4')
 
 # Parameters for Lucas-Kanade optical flow
 lk_params = dict(winSize=(15, 15),
@@ -20,39 +21,45 @@ prev_pts = cv2.goodFeaturesToTrack(prev_gray, maxCorners=100, qualityLevel=0.3, 
 # Initialize variables for speed calculation
 prev_x = prev_pts[:, 0, 0].mean()
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
-    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    # Calculate optical flow
-    new_pts, status, _ = cv2.calcOpticalFlowPyrLK(prev_gray, frame_gray, prev_pts, None, **lk_params)
-    
-    # Filter valid points and calculate mean movement on x-axis
-    new_pts_valid = new_pts[status == 1]
-    new_x = new_pts_valid[:, 0].mean()
-    
-    # Calculate speed on x-axis
-    speed_x = abs(new_x - prev_x)
-    
-    # Update previous frame and points
-    prev_gray = frame_gray.copy()
-    prev_pts = new_pts_valid.reshape(-1, 1, 2)
-    prev_x = new_x
-    
-    # Output speed
-    print("Speed along x-axis:", speed_x)
-    
-    # Display optical flow
-    for p in new_pts_valid:
-        x, y = p.ravel()
-    cv2.circle(frame, (int(x), int(y)), 5, color, -1)
 
-    cv2.imshow('Optical Flow', frame)
-    
-    if cv2.waitKey(30) & 0xFF == ord('q'):
-        break
+csv_file_path = 'speed_tracker.csv'
+with open(csv_file_path, 'w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+        # Calculate optical flow
+        new_pts, status, _ = cv2.calcOpticalFlowPyrLK(prev_gray, frame_gray, prev_pts, None, **lk_params)
+        
+        # Filter valid points and calculate mean movement on x-axis
+        new_pts_valid = new_pts[status == 1]
+        new_x = new_pts_valid[:, 0].mean()
+        
+        # Calculate speed on x-axis
+        speed_x = abs(new_x - prev_x)
+        
+        # Update previous frame and points
+        prev_gray = frame_gray.copy()
+        prev_pts = new_pts_valid.reshape(-1, 1, 2)
+        prev_x = new_x
+        
+        # Output speed
+        csv_writer.writerow([f'Speed along x-axis:{speed_x}'])
+        # print("Speed along x-axis:", speed_x)
+        
+        # Display optical flow
+        for p in new_pts_valid:
+            x, y = p.ravel()
+        cv2.circle(frame, (int(x), int(y)), 5, color, -1)
+
+        cv2.imshow('Optical Flow', frame)
+        
+        if cv2.waitKey(30) & 0xFF == ord('q'):
+            break
 
 cap.release()
 cv2.destroyAllWindows()
